1bcebe82ecf3a4f575cb4a354fececdb4ef2d829
easylogic <cyberuls@gmail.com>
2020-03-23 00:15:54 +0900

    support svelte button

--- src/summernote/SvelteSummernoteBS4.svelte
+++ src/summernote/SvelteSummernoteBS4.svelte
@@ -1,4 +1,4 @@
-<script >
+<script  context="module">
 import 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -6,6 +6,42 @@ import 'summernote/dist/summernote-bs4';
 import 'summernote/dist/summernote-bs4.css';
 
 import Summernote from '../component/Summernote';
+
+export function createSummernoteButton (opt = {}) {
+    return (context) => {
+        let buttonProps = {
+            container: opt.container || 'body',
+            tooltip: opt.tooltip || 'sample',
+        }
+
+        if (opt.title && !opt.element ) {
+            buttonProps.contents = opt.title;
+            buttonProps.click = () => {
+                if (opt && opt.onClick) {
+                    opt.onClick(context);
+                }
+            }
+            return context.ui.button(buttonProps).render();
+        } else {
+            const button = context.ui.button(buttonProps)
+            const el = button.render();   // return button as jquery object
+    
+            if (opt.element) {
+                const props = {context, ...opt.props};
+                const SvelteComponent = opt.element;
+                
+                new SvelteComponent({
+                    target: el[0],
+                    props,
+                });
+            }
+    
+        
+            return el;
+        }
+    }
+}
+
 
 </script>
 
